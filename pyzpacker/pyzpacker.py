cf39a5c773d7fc0390235e5e7e717f5c512f91ec
HUANG SIZHE <hsz1273327@gmail.com>
2024-06-20 09:38:22 +0800

    update

--- pyzpacker/pyzpacker.py
+++ pyzpacker/pyzpacker.py
@@ -70,7 +70,7 @@ def _delete_py_source(root_path: Path) -> None:
 
 
 def pyzpacker(source: str, main: str, *, output: Optional[str] = None, with_requirements: Optional[str] = None,
-              with_compress: bool = False, with_interpreter: bool = False, with_compile: bool = False):
+              with_compress: bool = False, with_interpreter: bool = False, with_compile: bool = False) -> None:
 
     cwd = Path.cwd()
     if output:
